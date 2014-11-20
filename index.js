c40f487880b0440398d640fbfa37e91fe24695e6
gdub22 <garth22@gmail.com>
2014-11-19 21:09:10 -0500

    serving static gzipped assets with serverMiddleware

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-var gzip = require('broccoli-gzip');
+var path       = require('path');
+var gzip       = require('broccoli-gzip');
+var gzipStatic = require('connect-gzip-static');
 
-var addonOptions;
+var addonEnabled; // need to store outside addon scope for serverMiddleware
+
+function setOptionOrDefault(option, defaultValue) {
+  return option === undefined ? defaultValue : option;
+}
 
 module.exports = {
   name: 'ember-cli-gzip',
-  initOptions: function() {
-    var options = addonOptions = this.options = this.app.options.gzip || {};
-    options.enabled          = options.enabled !== undefined ? options.enabled : this.app.env === 'production';
-    options.keepUncompressed = options.keepUncompressed !== undefined ? options.keepUncompressed : false;
-    options.appendSuffix     = options.appendSuffix !== undefined ? options.appendSuffix : true;
-    options.extensions       = options.extensions || ['js', 'css'];
-  },
   included: function(app) {
-    this.app = app;
-    this.initOptions();
+    var options = this.options = app.options.gzip || {};
+    addonEnabled = options.enabled = setOptionOrDefault(options.enabled, app.env === 'production');
+    options.keepUncompressed = setOptionOrDefault(options.keepUncompressed, false);
+    options.appendSuffix = setOptionOrDefault(options.appendSuffix, true);
+    options.extensions = setOptionOrDefault(options.extensions, ['js', 'css']);
   },
   postprocessTree: function (type, tree) {
     if (type === 'all' && this.options.enabled) {
       tree = gzip(tree, this.options);
     }
-
     return tree;
   },
   serverMiddleware: function(config) {
-    if (addonOptions.enabled) {
-      // TODO: add appropriate content headers for gz files
+    if (addonEnabled) {
+      var assetPath = path.join(config.options.project.root, config.options.outputPath);
+      config.app.use(gzipStatic(assetPath));
     }
   }
 };
